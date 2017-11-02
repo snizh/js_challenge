ef77bb2798d47a2cf14daf5a107076a0d843706b
Alexander <alexbtlv@gmail.com>
2017-11-02 11:07:42 +0500

    updated notes for task 3

--- task_3/mergeTwoLinkedLists.js
+++ task_3/mergeTwoLinkedLists.js
@@ -1,4 +1,8 @@
-// Note: You're not allowed to change ListNode defenition.
+//  Примечание
+// `l1` и `l2` не массив, вы не можете получить доступ к элементу по индексу `l1[0]`. 
+// Это Линейный связный список – https://ru.wikipedia.org/wiki/%D0%A1%D0%B2%D1%8F%D0%B7%D0%BD%D1%8B%D0%B9_%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA
+
+// `l1, l2` это `ListNode` и  имеет  только `.x` и `.next`. Декларация обьекта ниже. Вы не можете вносить изменения в объект.
 function ListNode(x) {
   this.value = x;
   this.next = null;
