0516da20b3df4839f4c9ebb99bf68ce42edbabdc
Sergei Nizhivenko <snizh@list.ru>
2017-11-16 08:11:04 +1000

    Для задачи 2 написал тестовую функцию

--- task_2/houseRobber.js
+++ task_2/houseRobber.js
@@ -1,3 +1,11 @@
+
 function houseRobber(nums) {
 
 }
+
+function houseRobberSpec() {
+  console.assert(houseRobber([5, 5, 5]) === 10, 'тест 1 сломался !');
+  console.assert(houseRobber([1, 3, 1, 3, 100]) === 103, 'тест 2 сломался !');
+}
+
+houseRobberSpec();
