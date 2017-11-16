d9584ca5f9c1737957d49034fada268e1f05465e
Sergei Nizhivenko <snizh@list.ru>
2017-11-16 13:30:51 +1000

    Первая версия слияния списков

--- task_3/mergeTwoLinkedLists.js
+++ task_3/mergeTwoLinkedLists.js
@@ -10,5 +10,46 @@ function ListNode(x) {
 
 
 function mergeTwoLinkedLists(l1, l2) {
+  const pickSmallest = (bothLists) => {
+    let sm;
 
+    if (!bothLists.l2 || bothLists.l1 && bothLists.l1.value <= bothLists.l2.value) {
+      sm = bothLists.l1;
+      bothLists.l1 = bothLists.l1.next;
+    } else {
+      sm = bothLists.l2;
+      bothLists.l2 = bothLists.l2.next;
+    };
+
+    return sm;
+  };
+
+  let lists = { l1, l2 };
+  const head = pickSmallest(lists);
+
+  let tail = head;
+
+  while (lists.l1 || lists.l2) {
+    tail.next = pickSmallest(lists);
+    tail = tail.next;
+  }
+
+  return head; 
 }
+
+function createList(arr) {
+  const node = new ListNode(arr[0]);
+ 
+  if (arr.length > 1) {
+    node.next = createList(arr.slice(1), node)
+  };
+  
+  return node;
+}
+
+function mergeTwoLinkedListsSpec() {
+  console.log(JSON.stringify(mergeTwoLinkedLists(createList([1, 2, 3]), createList([4, 5, 6])), null, 4));
+  console.log(JSON.stringify(mergeTwoLinkedLists(createList([5,10,15,40]), createList([2,3,20])), null, 4));
+}
+
+mergeTwoLinkedListsSpec();
